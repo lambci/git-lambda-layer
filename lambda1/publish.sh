d46e6628d8c2b21b9cd1ac93eb23d6d62d4471a8
Michael Hart <michael.hart.au@gmail.com>
2019-07-08 12:23:45 -0400

    Publish for all regions

--- lambda1/publish.sh
+++ lambda1/publish.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
 
+LAYER_NAME=git
+
 GIT_VERSION=2.21.0
 
-REGIONS='
-ap-northeast-1
-ap-northeast-2
-ap-south-1
-ap-southeast-1
-ap-southeast-2
-ca-central-1
-eu-central-1
-eu-west-1
-eu-west-2
-eu-west-3
-sa-east-1
-us-east-1
-us-east-2
-us-west-1
-us-west-2
-'
+REGIONS="$(aws ssm get-parameters-by-path --path /aws/service/global-infrastructure/services/lambda/regions \
+  --query 'Parameters[].Value' --output text | tr '[:blank:]' '\n' | grep -v -e ^cn- -e ^us-gov- | sort -r)"
 
 for region in $REGIONS; do
-  aws lambda add-layer-version-permission --region $region --layer-name git \
+  aws lambda add-layer-version-permission --region $region --layer-name $LAYER_NAME \
     --statement-id sid1 --action lambda:GetLayerVersion --principal '*' \
-    --version-number $(aws lambda publish-layer-version --region $region --layer-name git --zip-file fileb://layer.zip \
+    --version-number $(aws lambda publish-layer-version --region $region --layer-name $LAYER_NAME \
+      --zip-file fileb://layer.zip --cli-read-timeout 0 --cli-connect-timeout 0 \
       --description "Git ${GIT_VERSION} and openssh binaries" --query Version --output text)
 done
+
