f2cbf98693652e31ac0f002420ae893fcc2414d1
Michael Hart <michael.hart.au@gmail.com>
2019-12-10 14:41:57 -0500

    Update checks to read CodeSha256

--- lambda1/check.sh
+++ lambda1/check.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+# eg: ./check.sh 3
+
 LAYER_NAME=git
 
 REGIONS="$(aws ssm get-parameters-by-path --path /aws/service/global-infrastructure/services/lambda/regions \
   --query 'Parameters[].Value' --output text | tr '[:blank:]' '\n' | grep -v -e ^cn- -e ^us-gov- | sort -r)"
 
 for region in $REGIONS; do
-  echo $(curl -sSL $(aws lambda get-layer-version --region $region --layer-name $LAYER_NAME --version-number $1 \
-    --query Content.Location --output text 2>/dev/null) 2>/dev/null | wc -c) $region &
+  echo $(aws lambda get-layer-version --region $region --layer-name $LAYER_NAME --version-number $1 \
+    --query Content.CodeSha256 --output text 2>/dev/null) $region &
 done
 
 for job in $(jobs -p); do
