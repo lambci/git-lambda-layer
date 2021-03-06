d3a834b808f195cc05bd0eb8e84603d247d63e32
Chad Gilbert <chad.gilbert@cqlcorp.com>
2021-03-06 08:36:39 -0500

    Install specific version of git lfs
    At present, 2.12.1 is the latest version available from this RPM repo,
    even though there is a 2.13.2 on the git-lfs release page.

--- lambda2/build.sh
+++ lambda2/build.sh
@@ -7,7 +7,7 @@ rm layer.zip
 docker run --rm -v "$PWD":/tmp/layer lambci/yumda:2 bash -c "
   curl -sSf 'https://packagecloud.io/install/repositories/github/git-lfs/config_file.repo?os=amzn&dist=2&source=script' > /lambda/etc/yum.repos.d/github_git-lfs.repo && \
   yum -q makecache -y --disablerepo='*' --enablerepo='github_git-lfs' --enablerepo='github_git-lfs-source' && \
-  yum install -y git-${GIT_VERSION} git-lfs && \
+  yum install -y git-${GIT_VERSION} git-lfs-${GIT_LFS_VERSION} && \
   mv /lambda/usr/bin/git-lfs /lambda/opt/bin/git-lfs && \
   cd /lambda/opt && \
   zip -yr /tmp/layer/layer.zip .
--- lambda2/config.sh
+++ lambda2/config.sh
@@ -1,2 +1,3 @@
 export LAYER_NAME=git-lambda2
 export GIT_VERSION=2.29.0
+export GIT_LFS_VERSION=2.12.1
